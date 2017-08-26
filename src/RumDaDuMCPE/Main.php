d7edf5410404f3117a2a2511a08b79e074cb5403
RumBenCraftPE <ameensahal321@gmail.com>
2017-08-26 20:13:51 +0200

    Done and Done!

---
+++ src/RumDaDuMCPE/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RumDaDuMCPE;
+
+class Main extends \pocketmine\PluginBase implements \pocketmine\event\Listener {
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvent($this, $this);
+	}
+
+	public function onBreak(\pocketmine\event\block\BlockBreakEvent $event) {
+		if (($block = $event->getBlock())->getId() === 56) {
+			if (!(($player = $event->getPlayer())->isOp()) {
+				foreach ($this->getServer()->getOnlinePlayers() as $dudeordudette) {
+					if ($dudeordudette->isOp()) {
+						$dudeordudette->sendMessage("§l§a".$player->getName()." §eis trying to break a §bDiamond §eBlock!\n");
+					} else {
+						$this->getServer()->getLogger()->info($player->getName()." Mined a diamond block at coordinates: ".$block->getX().", ".$block->getY().", ".$block->getZ());
+					}
+				}
+			}
+		}
+	}
+}
+?>
